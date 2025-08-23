78133096a0771b8ec9b373c79a1431bdf3220947
rafipopz98 <itsrealrafi@gmail.com>
2025-08-23 14:23:58 +0530

    added folder structure

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,3 +3,7 @@ dotenv.config();
 
 export const PORT = <string>process.env.PORT;
 export const DB_URL = <string>process.env.DB_URL;
+export const JWT_TOKEN = <string>process.env.JWT_TOKEN;
+export const JWT_SECRET = new TextEncoder().encode(JWT_TOKEN);
+export const JWT_ALG = <string>process.env.ALG || "HS256";
+export const EXPIRES_IN = <string>process.env.EXPIRES_IN || "60 * 60 * 24 * 7";
